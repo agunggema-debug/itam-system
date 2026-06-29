47747846e7ab8b50a419fb6bc24a82ec2ac57be6
Your Name <agunggema@gmail.com>
2026-06-29 14:27:49 +0700

    feat: app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Illuminate\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,11 +18,12 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->trustProxies(
-            headers: TrustProxies::HEADER_X_FORWARDED_FOR |
-                     TrustProxies::HEADER_X_FORWARDED_HOST |
-                     TrustProxies::HEADER_X_FORWARDED_PORT |
-                     TrustProxies::HEADER_X_FORWARDED_PROTO |
-                     TrustProxies::HEADER_X_FORWARDED_AWS_ELB,
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                     Request::HEADER_X_FORWARDED_HOST |
+                     Request::HEADER_X_FORWARDED_PORT |
+                     Request::HEADER_X_FORWARDED_PROTO |
+                     Request::HEADER_X_FORWARDED_AWS_ELB,
         );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
