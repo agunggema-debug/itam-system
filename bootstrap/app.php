8c724cdb23d4fa6f4f6207eac3e325f9d4e80fd5
Your Name <agunggema@gmail.com>
2026-06-29 14:19:47 +0700

    feat : Procfile,railway,config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,6 +17,14 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
+
+        $middleware->trustProxies(
+            headers: TrustProxies::HEADER_X_FORWARDED_FOR |
+                     TrustProxies::HEADER_X_FORWARDED_HOST |
+                     TrustProxies::HEADER_X_FORWARDED_PORT |
+                     TrustProxies::HEADER_X_FORWARDED_PROTO |
+                     TrustProxies::HEADER_X_FORWARDED_AWS_ELB,
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
